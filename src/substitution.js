63b97420b362601c7650c596911a51e49a7739e9
Carolina Gomez <carolinagomezTX@gmail.com>
2021-10-06 12:01:23 -0500

    finished substitution function

--- src/substitution.js
+++ src/substitution.js
@@ -3,71 +3,60 @@
 // Only add code (e.g., helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
 
-// When building the function, keep the following constraints and rules in mind:
-
-// The input could include spaces and letters as well as special characters such as #, $, *, etc.
-// Spaces should be maintained throughout.
-// Capital letters can be ignored.
-// The alphabet parameter must be a string of exactly 26 characters, which could include special characters such as #, $, *, etc. Otherwise, it should return false.
-// All the characters in the alphabet parameter must be unique. Otherwise, it should return false.
-
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
   function substitution(input, alphabet, encode = true) {
     // your solution code here
+      let cipher = []
+      const usualABC = "abcdefghijklmnopqrstuvwxyz"
+      let output = ""
 
     //ERROR HANDLING
-        //should ignore cap letters
-        input = input.toLowerCase()
-        //needs alphabet param and has to be 26 chars otherwise ret false
-          if (!alphabet || alphabet.length !== 26){
+     //should ignore cap letters
+      input = input.toLowerCase()
+    //needs alphabet param and has to be 26 chars otherwise ret false
+      if (!alphabet || alphabet.length !== 26){
+        return false
+      }
+    //check for unique chars in alphabet param and add to cipher array
+    for (let i = 0; i < alphabet.length; i++){
+          if (cipher.includes(alphabet[i])){
             return false
-          }
-        //uique chars in alphabet param
-          for (let i = 0; i < alphabet.length; i++){
-            let index = 0
-            let letter = alphabet[index]
-
-            if (letter == alphabet[i]){
-              return false
-            } else {
-              index ++
-            }
-          }
-
-          //normal abc comparison
-          const usualABC = "abcdefghijklmnopqrstuvwxyz"
-          let output = ""
-          
-          //ENCODING
-          if (encode == true){
-            for(let i = 0; i < input.length; i++){
-              for (let j = 0; j < usualABC.length; j++){
-                if (input[i] == usualABC[j]){
-                  output += alphabet[j]
-                  console.log(output)
-                } else {
-                  output += input[i]
-                  console.log(output)
-                }
-              }
-            }
           } else {
-          // DECODING
+            cipher.push(alphabet[i])
+          }
+      i++
+    }
 
+    
+     //ENCODING
+          if (encode == true){
+           
             for(let i = 0; i < input.length; i++){
-              for (let j = 0; j < alphabet.length; j++){
-                if (input[i] == alphabet[j]){
-                  output += usualABC[j]
-                } else {
-                  output += input[i]
-                }
+              if (usualABC.includes(input[i])){
+                let alphaIndex = usualABC.indexOf(input[i])
+                output += alphabet[alphaIndex]
+              } else {
+                output += input[i]
               }
             }
+
+            return output 
+          } else {
+      //DECODING
+             for(let i = 0; i < input.length; i++){
+              if (alphabet.includes(input[i])){
+                let alphaIndex = alphabet.indexOf(input[i])
+                output += usualABC[alphaIndex]
+              } else {
+                output += input[i]
+              }
+            }
+             return output
           }
-          
-          return output
+    
+   
   }
 
   return {
@@ -76,3 +65,4 @@ const substitutionModule = (function () {
 })();
 
 module.exports = { substitution: substitutionModule.substitution };
+
