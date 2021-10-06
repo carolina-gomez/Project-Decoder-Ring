4f3225e92fa20358d58285f7acf542cb268ee5e5
Carolina Gomez <carolinagomezTX@gmail.com>
2021-10-06 12:45:21 -0500

    Added more caesar tests

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -12,6 +12,40 @@ describe("ceasar() function test", () => {
             const actual = caesar(input, shift)
             expect(actual).to.be.false
         })
-
+        it("If the shift value is less than -25, the function should return false ", () => {
+            const input = "thinkful"
+            const shift = -30
+            const actual = caesar(input, shift)
+            expect(actual).to.be.false
+        })
+        it("If the shift value is greater than 25, the function should return false ", () => {
+            const input = "thinkful"
+            const shift = 40
+            const actual = caesar(input, shift)
+            expect(actual).to.be.false
+        })
+    })
+    describe("encoding/decoding ", () => {
+        it("Encode correctly", () => {
+            const input = "thinkful"
+            const shift = 3
+            const actual = caesar(input, shift)
+            const expected = 'wklqnixo'
+            expect(actual).to.equal(expected)
+        })
+        it("Decode correctly", () => {
+            const input = "wklqnixo"
+            const shift = 3
+            const actual = caesar(input, shift, false)
+            const expected = 'thinkful'
+            expect(actual).to.equal(expected)
+        })
+        it("Spaces should be maintained throughout", () => {
+            const input = "This is a secret message!"
+            const shift = 8
+            const actual = caesar(input, shift)
+            const expected = 'bpqa qa i amkzmb umaaiom!'
+            expect(actual).to.equal(expected)
+        })
     })
 })
