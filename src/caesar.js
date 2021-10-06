9a0d0c1af7ee4afba48fbc105e62e7eaa17dd41d
Carolina Gomez <carolinagomezTX@gmail.com>
2021-10-06 12:37:58 -0500

    Added ceasar test and added param to check if no shift value was provided

--- src/caesar.js
+++ src/caesar.js
@@ -4,34 +4,61 @@
 // of the anonymous function on line 6
 
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
-
-
-  function caesar(input, shift, encode = true) {
-    // your solution code here
-    console.log(input);
-
-    
-    //SHOULD RETURN FALSE IF SHIFT IS TOO MUCH OR 0
-    if (shift === 0 || shift < -25 || shift > 25){
-      return false
-    }
-    //SHOULD IGNORE CAPITAL LETTERS
-    input = input.toLowerCase()
-    //store number values
-    charcode = []
-    output = ""
-
-     //check encode to know if decoding or encoding
-    if (encode === true){
-
-        //loop through input
-        for (let i = 0; i < input.length; i++){
+   // you can add any code you want within this function scope
+ 
+   function caesar(input, shift, encode = true) {
+      // your solution code here
+     console.log(input);
+ 
+     
+     //SHOULD RETURN FALSE IF SHIFT IS TOO MUCH OR 0
+     if (!shift || shift === 0 || shift < -25 || shift > 25){
+       return false
+     }
+     //SHOULD IGNORE CAPITAL LETTERS
+     input = input.toLowerCase()
+     //store number values
+     let charcode = []
+     let output = ""
+ 
+      //check encode to know if decoding or encoding
+     if (encode === true){
+ 
+         //loop through input
+         for (let i = 0; i < input.length; i++){
+              let number = input.charCodeAt(i)
+ 
+              //change each character by shift or unshift
+              let shiftedNumber = number + shift
+ 
+              if (number < 97 || number > 122){
+                 charcode.push(number)
+              } else if (shiftedNumber < 97){
+                 shiftedNumber = shiftedNumber + 26
+                 charcode.push(shiftedNumber)
+              } else if (shiftedNumber > 122) {
+                  shiftedNumber = shiftedNumber - 26
+                  charcode.push(shiftedNumber)
+              } else {
+                 charcode.push(shiftedNumber)
+              }
+         }
+ 
+         for (let i = 0; i < charcode.length; i++){
+             output += String.fromCharCode(charcode[i])
+         }
+ 
+         //return or console log result
+         console.log(output)
+         return output
+ 
+     } else {
+         //loop through input
+         for (let i = 0; i < input.length; i++){
              let number = input.charCodeAt(i)
-
              //change each character by shift or unshift
-             let shiftedNumber = number + shift
-
+             let shiftedNumber = number - shift
+ 
              if (number < 97 || number > 122){
                 charcode.push(number)
              } else if (shiftedNumber < 97){
@@ -44,48 +71,22 @@ const caesarModule = (function () {
                 charcode.push(shiftedNumber)
              }
         }
-
+ 
         for (let i = 0; i < charcode.length; i++){
             output += String.fromCharCode(charcode[i])
         }
-
+ 
         //return or console log result
         console.log(output)
         return output
-
-    } else {
-        //loop through input
-        for (let i = 0; i < input.length; i++){
-            let number = input.charCodeAt(i)
-            //change each character by shift or unshift
-            let shiftedNumber = number - shift
-
-            if (number < 97 || number > 122){
-               charcode.push(number)
-            } else if (shiftedNumber < 97){
-               shiftedNumber = shiftedNumber + 26
-               charcode.push(shiftedNumber)
-            } else if (shiftedNumber > 122) {
-                shiftedNumber = shiftedNumber - 26
-                charcode.push(shiftedNumber)
-            } else {
-               charcode.push(shiftedNumber)
-            }
-       }
-
-       for (let i = 0; i < charcode.length; i++){
-           output += String.fromCharCode(charcode[i])
-       }
-
-       //return or console log result
-       console.log(output)
-       return output
-    }
-  }
-
-  return {
-    caesar,
-  };
-})();
-
-module.exports = { caesar: caesarModule.caesar };
+     }
+   }
+ 
+   return {
+     caesar,
+   };
+ })();
+ 
+ module.exports = { caesar: caesarModule.caesar };
+ 
+ 
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,3 +1,17 @@
-// Write your tests here!
-//test
 
+// Write your tests here!
+
+const { expect } = require("chai");
+const { caesar } = require("../src/caesar")
+
+describe("ceasar() function test", () => {
+    describe("error handling", () => {
+        it("If the shift value is not present, the function should return false ", () => {
+            const input = "thinkful"
+            const shift = ""
+            const actual = caesar(input, shift)
+            expect(actual).to.be.false
+        })
+
+    })
+})
